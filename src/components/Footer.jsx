f1dec72497e6891b275bee2a9c7ffe0d32d1b073
Maheshbharambe45 <bharambemahesh7@gmail.com>
2026-01-10 10:12:18 +0530

    Updated src

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,8 @@ function Footer() {
         <div className='footer'>
             <div>
                 <h3>Company</h3>
-                <a href=""><h4>About us</h4></a>
-                <a href=""><h4>Careers</h4></a>
+                <a href="#"><h4>About us</h4></a>
+                <a href="#"><h4>Careers</h4></a>
                 <p style={{marginTop:"20px"}}>&copy;2023 STAR . All rights reserved</p>
                 <a href=''><p>Terms of Use &nbsp; Privacy  Policy &nbsp; FAQ </p></a>
             </div>
@@ -30,8 +30,8 @@ function Footer() {
                 <h3 style={{textAlign:"center"}}>Connect with Us</h3>
                 
                 <div className='ic'>
-                    <i class="fa-brands fa-facebook"></i>
-                    <i class="fa-brands fa-twitter"></i>
+                    <i className="fa-brands fa-facebook"></i>
+                    <i className="fa-brands fa-twitter"></i>
                 </div>
                 <div className='emg'>
                     <img src="https://o.remove.bg/downloads/6da3e562-e93e-46f4-bfa3-b9efd2809260/png-clipart-google-play-app-store-apple-apple-text-rectangle-removebg-preview.png" alt="" />
